f92923432f6acafb07d9d5ac7cec28b214526938
starboundstitch <personal@stitchy.moe>
2025-03-03 20:17:42 +0000

    feat: slinear11 format and tests

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,41 @@ pub mod ulinear16 {
     }
 }
 
+pub mod slinear11 {
+    pub fn from(val: f32, exp: i8) -> u16 {
+        let base: u32 = 2;
+        let lsb: f32;
+        if exp.is_positive() {
+            lsb = base.pow(exp as u32) as f32;
+        } else {
+            lsb = 1. / base.pow(exp.abs() as u32) as f32;
+        }
+
+        let mant: i32 = (val / lsb as f32) as i32;
+        let mant = mant & 0x07FF;
+        let exp: u32 = (exp & 0x1F) as u32;
+        return (mant as u32 | (exp << 11)) as u16;
+    }
+
+    pub fn to(val: u16) -> f32 {
+        let mut exp = (val >> 11) as i8;
+        if exp > 16 {
+            exp = exp - 32;
+        }
+        let mut mant: u32 = val as u32 & 0x000007FF;
+        if mant > 0x03FF {
+            mant = mant as u32 | 0xFFFFF800;
+        }
+
+        let base: u8 = 2;
+        // Workaround not having access to powi
+        if exp.is_positive() {
+            mant as f32 * base.pow(exp as u32) as f32
+        } else {
+            mant as f32 / base.pow(exp.abs() as u32) as f32
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -30,4 +65,27 @@ mod tests {
         assert_eq!(result, 1.0);
         assert_ne!(result, 1.2);
     }
+
+    #[test]
+    fn slinear11_to_float() {
+        let result = slinear11::to(0xE804);
+        assert_eq!(result, 0.5);
+        assert_ne!(result, 0.3);
+        assert_eq!(slinear11::to(0xE054), 5.25);
+    }
+
+    #[test]
+    fn float_to_slinear11() {
+        let result = slinear11::from(5.25, -4);
+        assert_eq!(result, 0xE054);
+        assert_ne!(result, 0xE000);
+    }
+
+    // #[test]
+    // fn chain() {
+    //     let val = 3.26;
+    //     let lin = slinear11::from(val, -4);
+    //     // Fails Due to accuracy constraints in slinear11 format
+    //     assert_eq!(slinear11::to(lin), val);
+    // }
 }
