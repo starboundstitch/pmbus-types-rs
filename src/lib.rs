332011e3475d4a2297821229f66ceafa50623913
starboundstitch <personal@stitchy.moe>
2025-03-04 02:32:59 +0000

    chore: fix linting
    Makes changes to fix the clippy errors.

--- src/lib.rs
+++ src/lib.rs
@@ -15,27 +15,26 @@ pub mod ulinear16 {
 pub mod slinear11 {
     pub fn from(val: f32, exp: i8) -> u16 {
         let base: u32 = 2;
-        let lsb: f32;
-        if exp.is_positive() {
-            lsb = base.pow(exp as u32) as f32;
+        let lsb: f32 = if exp.is_positive() {
+            base.pow(exp as u32) as f32
         } else {
-            lsb = 1. / base.pow(exp.abs() as u32) as f32;
-        }
+            1. / base.pow(exp.unsigned_abs() as u32) as f32
+        };
 
-        let mant: i32 = (val / lsb as f32) as i32;
+        let mant: i32 = (val / lsb) as i32;
         let mant = mant & 0x07FF;
         let exp: u32 = (exp & 0x1F) as u32;
-        return (mant as u32 | (exp << 11)) as u16;
+        (mant as u32 | (exp << 11)) as u16
     }
 
     pub fn to(val: u16) -> f32 {
         let mut exp = (val >> 11) as i8;
         if exp > 16 {
-            exp = exp - 32;
+            exp -= 32;
         }
         let mut mant: u32 = val as u32 & 0x000007FF;
         if mant > 0x03FF {
-            mant = mant as u32 | 0xFFFFF800;
+            mant |= 0xFFFFF800;
         }
 
         let base: u8 = 2;
@@ -43,7 +42,7 @@ pub mod slinear11 {
         if exp.is_positive() {
             mant as f32 * base.pow(exp as u32) as f32
         } else {
-            mant as f32 / base.pow(exp.abs() as u32) as f32
+            mant as f32 / base.pow(exp.unsigned_abs() as u32) as f32
         }
     }
 }
